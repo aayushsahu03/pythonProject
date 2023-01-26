99b1058cb3077ae1542bc67bfb5e642d5b8d88f9
aayushsahu03 <82887659+aayushsahu03@users.noreply.github.com>
2023-01-27 00:52:51 +0530

    commit -27th JAN by aayush

--- mail_merge/main.py
+++ mail_merge/main.py
@@ -1,5 +1,7 @@
-text = 'Hi [name], you have a pre-aprroved\n offer of Rs.1,00,000'
+text = 'Hi [name],\nyou have a pre-aprroved\noffer of Rs.1,00,000'
 with open("names_list.txt", 'r') as file:
     for name in file.readlines():
         with open(f"./{name.strip()}_letter.txt", 'w') as letter:
-            letter.write(text.replace('[name]',name))
+            letter.write(text.replace('[name]',name.strip()))
+
+
